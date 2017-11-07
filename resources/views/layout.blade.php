3e9dcca4fb2219ea986d6b1bb6ccac46fafdc9c1
Peter Nielson <computergeekpete@gmail.com>
2017-11-06 18:26:55 -0700

    Updated welcome page and layout page to link to internal pages

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -9,7 +9,7 @@
     <meta name="author" content="">
     <link rel="icon" href="favicon.ico">
 
-    <title>Starter Template</title>
+    <title>Greendale Community College</title>
 
     <!-- Bootstrap core CSS -->
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" rel="stylesheet">
@@ -37,7 +37,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="#">GREENDALE COMMUNITY COLLEGE</a>
+                <a class="navbar-brand" href="/">GREENDALE COMMUNITY COLLEGE</a>
             </div>
             <div id="navbar" class="collapse navbar-collapse">
                 <ul class="nav navbar-nav">
